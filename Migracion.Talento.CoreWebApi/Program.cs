f25e980b233494f8ecc8d9e0a1600da88d94efb6
Miguel Angel Cuevas <macuevi@gmail.com>
2025-02-24 18:33:13 -0600

    Se agrega el tipo de cultura ES para que se establezca en la app y no dependiente del servidor, para manejar las fechas

--- Migracion.Talento.CoreWebApi/Program.cs
+++ Migracion.Talento.CoreWebApi/Program.cs
@@ -1,6 +1,12 @@
 using Migracion.Talento.WebAPI;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configurar la cultura predeterminada a "es-ES"
+var cultureInfo = new CultureInfo("es-ES");
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
 
 var startup = new Startup(builder.Configuration);
 startup.ConfiurationServices(builder.Services);
